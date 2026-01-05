ecc3232546c68efcd08dfd05d2a03bfc4fe6956e
kimjunsseop <jskim33350@gmail.com>
2026-01-05 10:14:29 +0900

    firstcommit
    testcommit

---
+++ lion7/test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string state = "WEAPON UNLOCKS";
+            string gunName = "M4A1";
+            string pageName = "AFTER ACTION REPORT";
+            string detailOne = "Lv.3";
+            string detailTwo = "Lv.4";
+            string detailThree = "Lv.5";
+            string itemOne = "shoes";
+            string itemTwo = "builet";
+            string itemThree = "health";
+            Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine($"┃     {pageName}                                                       ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine($"┃                                      {gunName}                                     ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine($"┃             {itemOne}                    {itemTwo}               {itemThree}              ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine($"┃              {detailOne}                    {detailTwo}                 {detailThree}                ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┃                                                                               ┃");
+            Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+        }
+    }
+}
