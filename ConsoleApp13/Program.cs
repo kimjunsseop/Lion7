91654507bde9885e232bbcf0641b28108620df26
kimjunsseop <jskim33350@gmail.com>
2026-01-08 15:02:37 +0900

    0108_class

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new B();
+            A c = new B();
+            a.shoot();
+            b.shoot();
+            c.shoot();
+            c.hello();
+
+        }
+    }
+    class A
+    {
+        public virtual void shoot()
+        {
+            Console.WriteLine("parent shoot");
+        }
+        public virtual void hello()
+        {
+            Console.WriteLine("parent hello");
+        }
+    }
+
+    class B : A
+    {
+        public override void shoot()
+        {
+            Console.WriteLine("child shoot");
+        }
+        public void hello()
+        {
+            Console.WriteLine("child hello");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 10;
+            ConsoleKeyInfo keyInfo;
+
+            Console.CursorVisible = false;
+
+            while(true)
+            {
+                Console.Clear();
+
+                Console.SetCursorPosition(x, y);
+
+                Console.Write("●");
+                keyInfo = Console.ReadKey(true); // 키 입력받기
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (y > 0) y--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if(y < Console.WindowHeight - 1) y++;
+                        break ;
+                    case ConsoleKey.LeftArrow:
+                        if(x > 0) x--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if(x < Console.WindowWidth - 1) x++;
+                        break ;
+                    case ConsoleKey.Spacebar:
+                        Console.Write("미사일키");
+                        break;
+                    case ConsoleKey.Escape:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
